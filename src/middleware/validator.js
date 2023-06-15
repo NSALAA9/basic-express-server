67873a4422e16605dfcda3bd0e206277a536e4a7
NSALAA9 <alaansairat99@gmail.com>
2023-06-15 04:57:11 +0000

    updates

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,9 +1,25 @@
+// const validator = (req, res, next) => {
+//     const name = req.query.name;
+//     console.log(typeof name, ' ', name)
+//     if(!isNaN(name)) next({message: 'Not a string'});
+//     else if(name.trim().length() == 0) next({message: 'Empty'});
+//   req.name = name
+//   next()
+//   };
+  
+//   module.exports = validator;
+  
+
 const validator = (req, res, next) => {
     const name = req.query.name;
     console.log(typeof name, ' ', name)
-    if(!isNaN(name)) next({message: 'Not a string'});
-  req.name = name
-  next()
+    if (!isNaN(name)) {
+      next({ message: 'Not a string' });
+    } else if (name.trim().length === 0) {
+      next({ message: 'Empty' });
+    }
+    req.name = name;
+    next();
   };
   
   module.exports = validator;
